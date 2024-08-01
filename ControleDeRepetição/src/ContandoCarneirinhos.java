e6d068afc6429c0afe7b1ecbdd694c11013105e0
Eliseu <eliseu.souza@mail.usf.edu.br>
2024-08-01 13:14:01 -0300

    Controle de repetição, contando carneirinhos com for

---
+++ ControleDeRepetição/src/ContandoCarneirinhos.java
@@ -0,0 +1,9 @@
+public class ContandoCarneirinhos {
+    public static void main(String[] args) {
+        
+        //Contando carneirinhos com for expressao basica
+        for(int carneiro = 1; carneiro <= 20; carneiro++) {
+            System.out.println(carneiro + " Carneirinho");
+        }
+    }
+}
