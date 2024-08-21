e24eb7c7b0549333a054a134260bfdb247932416
Eliseu <eliseu.souza@mail.usf.edu.br>
2024-08-21 14:50:03 -0300

    criando uma arraylist

---
+++ Arraylist/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //contruindo arraylist para carros.
+        ArrayList<String> carros = new ArrayList<>();
+
+
+    }
+}
